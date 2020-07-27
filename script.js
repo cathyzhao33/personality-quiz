436a47384ea4aa77a14026d363eb0d18e89bf8ff
Glitch (personality-quiz-starter-code) <none>
2020-07-27 16:12:15 +0000

    🏂🐊 Checkpoint ./index.html:19184803/5 ./script.js:19184803/1306

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var moodyScore = 0;
 var festiveScore = 0;
 var homeyScore = 0;
 var modernScore = 0;
+var moodyScore = 0;
 
 //#TODO: Use the DOM to create variables for the first quiz question.
 var q1a1 = document.getElementById("q1a1");
@@ -22,32 +23,82 @@ var q3a2 = document.getElementById("q3a2");
 var q3a3 = document.getElementById("q3a3");
 var q3a4 = document.getElementById("q3a4");
 
+var result = document.getElementById("result");
+
 
 //#TODO: Add Event Listeners to your answer choice variables.
 q1a1.addEventListener("click", resultMoody);
+q1a2.addEventListener("click", resultFestive);
+q1a3.addEventListener("click", resultHomey);
+q1a4.addEventListener("click", resultModern);
+
+q2a1.addEventListener("click", resultFestive);
+q2a2.addEventListener("click", resultModern);
+q2a3.addEventListener("click", resultMoody);
+q2a4.addEventListener("click", resultHomey);
+
+q3a1.addEventListener("click", resultHomey);
+q3a2.addEventListener("click", resultMoody);
+q3a3.addEventListener("click", resultModern);
+q3a4.addEventListener("click", resultFestive);
 
 
 
 // Define quiz functions here
-function resultMoody(){
+function resultMoody() {
   moodyScore++;
   questionCount++;
-  if(questionCount >= 3) {
+  alert("moody+1");
+  
+  if (questionCount >= 3) {
     updateResult();
   }
 }
 
-function resultFestive(){
+function resultFestive() {
   festiveScore++;
   questionCount++;
+  alert("festive+1");
+  
+  if (questionCount >= 3) {
+    updateResult();
+  }
 }
 
-function resultModern(){
+function resultModern() {
   modernScore++;
   questionCount++;
+  alert("modern+1");
+  
+  if (questionCount >= 3) {
+    updateResult();
+  }
 }
 
-function resultHomey(){
+function resultHomey() {
   homeyScore++;
   questionCount++;
+  alert("homey+1");
+  
+  if (questionCount >= 3) {
+    updateResult();
+  }
+}
+
+function updateResult() {
+  if (moodyScore >= 2) {
+    result.innerHTML = "Moody and Mysterious"
+  }
+  else if (homeyScore >= 2) {
+    
+  }
+  else if (festiveScore >= 2) {
+    
+  }
+  else if (modernScore >= 2) {
+    
+  }
+  else {
+    
+  }
 }
